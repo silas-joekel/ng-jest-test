8fbecbd207f7d346c940b92588cb0fdc91a5b357
Silas Jökel <silas.joekel@elevait.de>
2021-12-03 04:55:44 +0100

    Implement simple todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,56 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+type Todo = {
+  title: string;
+};
+
+export const mockTodos = [
+  { title: "Learn Angular" },
+  { title: "Add testing with Jest" },
+  { title: "Have fun" },
+];
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
+    <button (click)="addTodo()">Add Todo</button>
     <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      <li *ngFor="let todo of todos$ | async">
+        {{ todo.title }}
+        <button (click)="removeTodo(todo)">X</button>
       </li>
     </ul>
-    
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-  title = 'ng-jest-test';
+  private _todos = new BehaviorSubject<Todo[]>(mockTodos);
+  todos$ = this._todos.asObservable();
+
+  addTodo(): void {
+    const currentTodos = this._todos.getValue();
+
+    if (currentTodos.length === 0) {
+      const newTodos = [{title: 'First Todo \\( ﾟヮﾟ)/'}]
+      this._todos.next(newTodos);
+    } else {
+      const additionalTodo = {title: `Todo #${currentTodos.length}`};
+      const newTodos = currentTodos.concat(additionalTodo);
+      this._todos.next(newTodos);
+    }
+  }
+
+  removeTodo(todo: Todo): void {
+    const currentTodos = this._todos.getValue();
+
+    /*
+     * With this implementation there may be cases
+     * where multiple todos with the same title are deleted.
+     * In production one should use unique ids.
+     */
+    const newTodos = currentTodos.filter(t => t.title !== todo.title);
+
+    this._todos.next(newTodos);
+  }
 }

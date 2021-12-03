0c43a0b1125458cb24a9b49073e301f664cf15a8
Silas Jökel <silas.joekel@elevait.de>
2021-12-03 06:06:52 +0100

    Test app component functions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent, Todo } from './app.component';
+
+export const mockTodos: Todo[] = [
+  { title: "Learn Angular" },
+  { title: "Add testing with Jest" },
+  { title: "Have fun" },
+];
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -22,4 +28,42 @@ describe('AppComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('addTodo', () => {
+    it('should append one todo containing the previous length of the array as number', () => {
+      const todosGetValueSpy = jest.spyOn(component['_todos'], 'getValue').mockReturnValue(mockTodos);
+      const todosNextSpy = jest.spyOn(component['_todos'], 'next').mockImplementation();
+
+      component.addTodo();
+
+      expect(todosGetValueSpy).toBeCalledTimes(1);
+      expect(todosNextSpy).toBeCalledTimes(1);
+      expect(todosNextSpy).toBeCalledWith([...mockTodos, {title: 'Todo #3'}]);
+    });
+
+    it('should add item with title \'First Todo \\( ﾟヮﾟ)/\' if todos array is empty', () => {
+      const todosGetValueSpy = jest.spyOn(component['_todos'], 'getValue').mockReturnValue([]);
+      const todosNextSpy = jest.spyOn(component['_todos'], 'next').mockImplementation();
+
+      component.addTodo();
+
+      expect(todosGetValueSpy).toBeCalledTimes(1);
+      expect(todosNextSpy).toBeCalledTimes(1);
+      expect(todosNextSpy).toBeCalledWith([{title: 'First Todo \\( ﾟヮﾟ)/'}]);
+    });
+  });
+
+  describe('removeTodo', () => {
+    it('should filter out todo by title', () => {
+      const todosGetValueSpy = jest.spyOn(component['_todos'], 'getValue').mockReturnValue(mockTodos);
+      const todosNextSpy = jest.spyOn(component['_todos'], 'next').mockImplementation();
+
+      component.removeTodo(mockTodos[1]);
+
+      expect(todosGetValueSpy).toBeCalledTimes(1);
+      expect(todosNextSpy).toBeCalledTimes(1);
+      // New array should only contain the first and third todo, not the second one
+      expect(todosNextSpy).toBeCalledWith([mockTodos[0], mockTodos[2]]);
+    })
+  })
 });

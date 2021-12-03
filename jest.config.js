0e1288e799ec7912c2d77bd325d2e44f96681109
Silas Jökel <silas.joekel@elevait.de>
2021-12-03 05:41:33 +0100

    Add coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    }
+  }
 };
